64c9fffa785a60841473888dea00d283ff0b2c5e
arman <salimi.dakaweb@gmail.com>
2026-08-17 17:31:50 +0330

    crate stayle and dy nav

--- app/comp/Nav.tsx
+++ app/comp/Nav.tsx
@@ -1,14 +1,36 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { title } from 'process'
 
 
 function Nav() {
-  return (
-    <div>
-        <Link href="/">Home</Link>
-        <Link href="/store">store</Link>
-        <Link href="/about">about</Link>
-    </div>
-  )
+    const pathname = usePathname()
+
+    const NavLinks = [
+        {
+            href: "/",
+            title: "Home"
+        }, {
+            href: "/Store",
+            title: "Store"
+        }, {
+            href: "/About",
+            title: "About"
+        }
+
+    ]
+    return (
+        <nav className='shadow p-4'>
+            {
+                NavLinks.map((
+                    item => (
+                        <Link className={`pr-2 ${pathname === item.href ? "text-blue-700 font-bold text-2xl" : ""} `}
+                            key={item.href} href={item.href}>{item.title}</Link>
+                    )))
+            }
+        </nav>
+    )
 }
 
 export default Nav
