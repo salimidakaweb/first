248a11d3ab9bb4002ef2d499f411cdfcfdad66a9
arman <salimi.dakaweb@gmail.com>
2026-08-19 16:31:35 +0330

    style cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,11 +7,17 @@ export default function Cart() {
       <div>
         <h1 className="pb-2">cart item</h1>
 
-          <CartItems />
-          <CartItems />
-          <CartItems />
-          <CartItems />
+        <CartItems />
+      </div>
+      <div className="border shadow-md p-4">
+        <h3>total price: 100$</h3>
+        <h3>win: 10$</h3>
+        <h3>final price: 90$</h3>
+        <div className="mt-2">
+          <input className="border" placeholder="trial code" type="number" />
+          <button className="bg-amber-300 rounded ml-1 px-1">submit</button>
         </div>
+      </div>
 
     </Cont>
   );
